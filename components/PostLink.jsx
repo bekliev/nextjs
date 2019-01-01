ac2e5326c2384910ce9309c79a40da6623206e9e
Ibragim Bekliev <nightkon95@yandex.ru>
2019-01-01 14:33:38 +0300

    Deleted useless files

--- components/PostLink.jsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
--- pages/blog/BlogPostLink.jsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default props => (
-  <li>
-    <Link as={`/blog/${props.id}`} href={`/blog/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
