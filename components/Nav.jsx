72034eb47a69063d12381c1901d54a1c0b51d7c6
Ibragim Bekliev <nightkon95@yandex.ru>
2019-01-03 00:22:24 +0300

    Disable prefetch nav links

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,29 +2,24 @@ import Link from 'next/link';
 
 const Nav = () => (
   <nav>
-    <style>
-      {`
-        nav a {
-          text-transform: uppercase;
-        }
-        nav a:not(:first-child) {
-          margin-left: 10px;
-        }
-      `}
-    </style>
-    <Link prefetch href="/">
+    <style jsx>{`
+      nav a {
+        text-transform: uppercase;
+      }
+      nav a:not(:first-child) {
+        margin-left: 15px;
+      }
+    `}</style>
+    <Link href="/">
       <a>Home</a>
     </Link>
-    <Link prefetch href="/about">
-      <a>About</a>
-    </Link>
-    <Link prefetch href="/days">
-      <a>Days</a>
-    </Link>
-    <Link prefetch href="/blog">
+    <Link href="/blog">
       <a>Blog</a>
     </Link>
-    <Link prefetch href="/tv-shows">
+    <Link href="/about">
+      <a>About</a>
+    </Link>
+    <Link href="/tv-shows">
       <a>TV Shows</a>
     </Link>
   </nav>
