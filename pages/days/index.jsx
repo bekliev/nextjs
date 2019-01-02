0901b184432564a0904bb1d4b1be8e7c1e825b5f
Ibragim Bekliev <nightkon95@yandex.ru>
2019-01-03 00:21:48 +0300

    Delete useless days page

--- pages/days/index.jsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../../components/Layout';
-
-const DaysCategory = () => (
-  <Layout title="Days!">
-    <p>Working days are ...</p>
-  </Layout>
-);
-
-export default DaysCategory;
