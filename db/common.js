adbb8c2a9e95d5e9d482ce67477bc0bcba574d21
Ibragim Bekliev <nightkon95@yandex.ru>
2019-01-11 00:25:45 +0300

    Make a simple db file

---
+++ db/common.js
@@ -0,0 +1,10 @@
+const common = {
+  pages: [
+    { title: 'Welcome to NextJS!', url: '/', exact: true },
+    { title: 'Blog Next.js', url: '/blog', exact: false },
+    { title: 'About us!', url: '/about', exact: true },
+    { title: 'Batman TV Shows', url: '/tv-shows', exact: false }
+  ]
+};
+
+export { common as default };
