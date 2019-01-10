b335b20be1bf68132c9de20e1d88f6e81f45979a
Ibragim Bekliev <nightkon95@yandex.ru>
2019-01-11 00:46:00 +0300

    Move blog posts db to a simple db-file

---
+++ db/blog-posts-db.js
@@ -0,0 +1,13 @@
+const blogPosts = [
+  { id: '1-post', name: 'Getting started' },
+  { id: '2-post', name: 'Navigate Between Pages' },
+  { id: '3-post', name: 'Using Shared Components' },
+  { id: '4-post', name: 'Create Dynamic Pages' },
+  { id: '5-post', name: 'Clean URLs with Route Masking' },
+  { id: '6-post', name: 'Server Side Support for Clean URLs' },
+  { id: '7-post', name: 'Fetching Data for Pages' },
+  { id: '8-post', name: 'Styling Components' },
+  { id: '9-post', name: 'Deploying a Next.js App' }
+];
+
+export { blogPosts as default };
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,18 +1,6 @@
 import PostLink from '../../components/PostLink';
 
-function getBlogPosts() {
-  return [
-    { id: '1-post', name: 'Getting started' },
-    { id: '2-post', name: 'Navigate Between Pages' },
-    { id: '3-post', name: 'Using Shared Components' },
-    { id: '4-post', name: 'Create Dynamic Pages' },
-    { id: '5-post', name: 'Clean URLs with Route Masking' },
-    { id: '6-post', name: 'Server Side Support for Clean URLs' },
-    { id: '7-post', name: 'Fetching Data for Pages' },
-    { id: '8-post', name: 'Styling Components' },
-    { id: '9-post', name: 'Deploying a Next.js App' }
-  ];
-}
+import data from '../../db/blog-posts-db';
 
 const BlogPage = () => (
   <>
